67351227df9e2de3d9ea9c4f552d70c0d5c635fe
kiyanisaab046-hub <kiyanisaab046@gmail.com>
2026-05-19 20:34:46 +0500

    perf: replace Lenis scroll-jacking with native smooth scrolling to fix background raf loops

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,28 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
+    // Enable browser-native smooth scrolling
+    try {
+      document.documentElement.style.scrollBehavior = "smooth";
+    } catch (e) {}
     return () => {
-      lenis.destroy();
+      try {
+        document.documentElement.style.scrollBehavior = "";
+      } catch (e) {}
     };
   }, []);
 
