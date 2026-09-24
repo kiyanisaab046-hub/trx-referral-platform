495a34bb0ab164d196a322ac64c8d4a646144918
kiyanisaab046-hub <kiyanisaab046@gmail.com>
2026-09-24 19:47:59 +0500

    Update Monetag zoneId to 11881405

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11881234
+    "zoneId": 11881405
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
